373f5c8e0af25c89bfd74d2e1a90bf8979af3d6b
Fred Blue <yyyguy@gmail.com>
2021-10-08 15:23:50 -0700

    initial commit

--- src/com/bci/Main.java
+++ src/com/bci/Main.java
@@ -3,6 +3,8 @@ package src.com.bci;
 public class Main {
 
     public static void main(String[] args) {
+
+        // This is just a test.
         System.out.println("Goodbye Cruel World");
         
 	// write your code here
