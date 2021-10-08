5f421944dd9e7ae1d4247946fcf92bf8e16ee1c4
Fred Blue <yyyguy@gmail.com>
2021-10-08 15:22:42 -0700

    initial commit

---
+++ src/com/bci/Main.java
@@ -0,0 +1,10 @@
+package src.com.bci;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Goodbye Cruel World");
+        
+	// write your code here
+    }
+}
